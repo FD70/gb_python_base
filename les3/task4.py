49b2d54aef4c63732c8761ab04b67a310ae655d1
FD70000000 <halfl.ez.ser@mail.ru>
2020-09-02 23:13:01 +0300

    lesson_3 release task4.py

--- les3/task4.py
+++ les3/task4.py
@@ -6,3 +6,26 @@
 # Подсказка: попробуйте решить задачу двумя способами.
 # Первый — возведение в степень с помощью оператора **.
 # Второй — более сложная реализация без оператора **, предусматривающая использование цикла.
+
+def my_func_h(x, y):
+    return x ** y
+
+
+def my_func(x, y):
+    if not y:
+        return 1
+    if not x:
+        if y < 0:
+            raise ZeroDivisionError('0.0 cannot be raised to a negative power')
+        return 0
+
+    temp = x
+    for i in range(abs(y) - 1):
+        temp *= x
+    return temp if y >= 0 else 1/temp
+
+
+xx, yy = map(int, input("Ввод: ").split())
+
+print(my_func_h(xx, yy))
+print(my_func(xx, yy))
