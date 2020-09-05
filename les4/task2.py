b17f362978bf1a852cfdae877d236a00cb5fc4e7
FD70000000 <halfl.ez.ser@mail.ru>
2020-09-05 22:48:33 +0300

    lesson_4 release task2.py

--- les4/task2.py
+++ les4/task2.py
@@ -1,3 +1,30 @@
+import random
+
+
+def get_random_list():
+    for el in range(10):
+        yield random.randint(1, 100)
+
+
+random_list = get_random_list()
+
+start_list = []
+result = []
+
+temp = next(random_list)
+start_list.append(temp)
+
+for i in random_list:
+    if temp >= i:
+        temp = i
+    else:
+        temp = i
+        result.append(i)
+    start_list.append(i)
+
+print(start_list)
+print(result)
+
 # 2. Представлен список чисел.
 # Необходимо вывести элементы исходного списка, значения которых больше предыдущего элемента.
 # Подсказка: элементы, удовлетворяющие условию, оформить в виде списка.
