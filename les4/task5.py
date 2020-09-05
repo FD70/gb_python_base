15ea4764179b986ed3a93940f6c6149555f898e3
FD70000000 <halfl.ez.ser@mail.ru>
2020-09-05 23:11:10 +0300

    lesson_4 release task5.py

--- les4/task5.py
+++ les4/task5.py
@@ -1,3 +1,12 @@
+import random
+import functools
+
+
+random_list = [random.randrange(100, 1000) for i in range(2)]
+print(random_list)
+
+print(functools.reduce(lambda x, y: x * y, random_list))
+
 # 5. Реализовать формирование списка, используя функцию range() и возможности генератора.
 # В список должны войти четные числа от 100 до 1000 (включая границы).
 # Необходимо получить результат вычисления произведения всех элементов списка.
