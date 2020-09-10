6c79c85c504b4f0c2184b1e8116f6bbee2722288
FD70000000 <halfl.ez.ser@mail.ru>
2020-09-10 15:22:55 +0300

    lesson_5 release task2.py

--- les5/task2.py
+++ les5/task2.py
@@ -1,3 +1,19 @@
+endict = {
+    1: "слово",
+    2: "слова",
+    3: "слова",
+    4: "слова",
+}
+
+with open("created/text_1.txt", "r", encoding="utf-8") as mfile:
+    for count, line in enumerate(mfile):
+        # print(line.replace("\n", ""))
+        line = line.replace("\n", "")
+
+        temp_c = len(line.split())
+        temp = "слов" if temp_c not in endict else endict[temp_c]
+
+        print(f"{count + 1}: {temp_c} {temp} - {line}")
 
 # 2. Создать текстовый файл (не программно),
 # сохранить в нем несколько строк, выполнить подсчет количества строк, количества слов в каждой строке.
