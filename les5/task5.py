e6d1711381666f01588cbc5f92b5810809fa08c0
FD70000000 <halfl.ez.ser@mail.ru>
2020-09-10 16:10:23 +0300

    lesson_5 release task5.py

--- les5/task5.py
+++ les5/task5.py
@@ -1,3 +1,10 @@
+with open("created/text_5.txt", "w", encoding="utf-8") as mfile:
+    mfile.write(" ".join(map(str, list(range(1, 11)))))
+
+with open("created/text_5.txt", "r", encoding="utf-8") as mfile:
+    string_list = mfile.readline().replace("\n", "").split()
+    summm = sum(map(int, string_list))
+    print(f"sum: {summm}")
 
 # 5. Создать (программно) текстовый файл,
 # записать в него программно набор чисел, разделенных пробелами.
