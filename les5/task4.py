aab3b304f6c85b836d8b28ba9cc4da9f9b888d5f
FD70000000 <halfl.ez.ser@mail.ru>
2020-09-10 15:55:10 +0300

    lesson_5 release task4.py

--- les5/task4.py
+++ les5/task4.py
@@ -1,3 +1,17 @@
+endict = {
+    "One": "Один",
+    "Two": "Два",
+    "Three": "Три",
+    "Four": "Четыре",
+}
+
+# One - 1
+with open("res/text_4.txt", "r", encoding="utf-8") as input_file:
+    with open("created/text_4_out.txt", "w", encoding="utf-8") as output_file:
+        for line in input_file:
+            for key in endict:
+                line = line.replace(key, endict[key])
+            output_file.write(line)
 
 # 4. Создать (не программно) текстовый файл со следующим содержимым:
 # One — 1
