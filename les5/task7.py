71eb514169c83908b6b1e3d059ba4a7c186299a0
FD70000000 <halfl.ez.ser@mail.ru>
2020-09-10 17:18:10 +0300

    lesson_5 release task7.py

--- les5/task7.py
+++ les5/task7.py
@@ -1,3 +1,29 @@
+import json
+
+comps = {}
+av_prof = {}
+output_list = [comps, av_prof]
+
+# Brooms ПАО 2500000 500000
+with open("res/text_7.txt", "r", encoding="utf-8") as mfile:
+    # Вся прибыль неубыточных компаний, их количество
+    total_suc_prof = 0
+    tsc_count = 0
+
+    for line in mfile:
+        name, mtype, f_plus, f_minus = line.split()
+        f_plus = int(f_plus)
+        f_minus = int(f_minus)
+
+        comps[name] = f_plus - f_minus
+        if f_plus > f_minus:
+            tsc_count += 1
+            total_suc_prof += f_plus - f_minus
+
+    av_prof["average_profit"] = total_suc_prof / tsc_count
+
+with open("created/text_7_out.json", "w", encoding="utf-8") as output_file:
+    json.dump(output_list, output_file, ensure_ascii=False, indent=4)
 
 # 7. Создать (не программно) текстовый файл,
 # в котором каждая строка должна содержать данные о фирме: название, форма собственности, выручка, издержки.
