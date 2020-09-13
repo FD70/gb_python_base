05681d0ca54f9d3afb323666110e605c2a962ce0
FD70000000 <halfl.ez.ser@mail.ru>
2020-09-13 15:07:18 +0300

    lesson_6 release task4.py

--- les6/task4.py
+++ les6/task4.py
@@ -1,3 +1,73 @@
+import random
+# Направления
+DIRS = ("Лево", "Право", "Назад")
+
+
+class Car:
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        print(f"{self.name} поехала")
+
+    def stop(self):
+        print(f"{self.name} остановилась")
+
+    def turn(self, direction):
+        print(f"{self.name} повернула {direction}")
+
+    def show_speed(self):
+        print(f"Текущаяя скорость {self.name} - {self.speed}")
+
+
+class TownCar(Car):
+    _speed_limit = 60
+
+    def show_speed(self):
+        print(f"Текущаяя скорость {self.name} - {self.speed}")
+        if self.speed > self._speed_limit:
+            print(f"Превышение скорости для {self.name}")
+
+
+class SportCar(Car):
+    pass
+
+
+class WorkCar(Car):
+    _speed_limit = 40
+
+    def show_speed(self):
+        print(f"Текущаяя скорость {self.name} - {self.speed}")
+        if self.speed > self._speed_limit:
+            print(f"Превышение скорости для {self.name}")
+
+
+class PoliceCar(Car):
+    def __init__(self, speed, color, name, is_police, car):
+        super().__init__(speed, color, name, is_police)
+        self.purpose = car
+
+    def go(self):
+        print(f"{self.name} выдвинулся за {self.purpose.name}")
+
+
+work1 = WorkCar(42, "Белый", "Грузовик", False)
+work2 = WorkCar(30, "Синий", "Камаз", False)
+
+work1.go()
+work2.go()
+
+work2.show_speed()
+work1.show_speed()
+
+police1 = PoliceCar(120, "Черный", "PoliceCar1", True, work1)
+police1.go()
+
+work1.turn(random.choice(DIRS))
+police1.turn(random.choice(DIRS))
 
 # 4. Реализуйте базовый класс Car.
 # У данного класса должны быть следующие атрибуты: speed, color, name, is_police (булево).
