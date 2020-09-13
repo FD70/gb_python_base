8de2d55952afd757ad740cf3df338e51fd734aec
FD70000000 <halfl.ez.ser@mail.ru>
2020-09-13 13:31:08 +0300

    lesson_6 release task1.py

--- les6/task1.py
+++ les6/task1.py
@@ -1,3 +1,40 @@
+from time import sleep, time
+import itertools
+
+
+class TrafficLight:
+    __TLcolors = ("Red", "Yellow", "Green")
+    # __next_color = iter(__TLcolors)
+    __next_color = itertools.cycle(__TLcolors)
+    __color = next(__next_color)
+
+    def __change_color(self):
+        if self.__color == "Red":
+            print(self.__color)
+            sleep(7)
+        elif self.__color == "Yellow":
+            print(self.__color)
+            sleep(2)
+        elif self.__color == "Green":
+            print(self.__color)
+            sleep(3)
+        else:
+            print("err")
+        self.__color = next(self.__next_color)
+
+    # было предложение, использовать метод "sleep",
+    # моделирования поведения светофора
+    def running(self):
+        # Светофор будет работать 30 секунд
+        time_shutdown = int(time())
+        while int(time_shutdown + 30 > time()):
+            self.__change_color()
+            print(int(time() - time_shutdown))
+        print("Работа светофора завершена")
+
+
+m_svet = TrafficLight()
+m_svet.running()
 
 # 1. Создать класс TrafficLight (светофор)
 # и определить у него один атрибут color (цвет) и метод running (запуск).
